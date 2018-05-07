29616972a038a73c4259ba64df76ce7804233614
Rhesa Utomo <venderblitz@gmail.com>
2018-05-07 10:37:05 +0700

    first commit

--- main.js
+++ main.js
@@ -3,3 +3,8 @@
 const Calculator = require("./calculator.js")
 
 // execute function on calculator.js in here
+var number = new Calculator();
+var number2 = new Calculator();
+number.add(9).substract(3).divide(2).square();
+number2.add(5).power(3).pi();
+console.log(number, number2);
